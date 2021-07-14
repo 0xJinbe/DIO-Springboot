952e0b2933cc1919b92e80611d0bc4e5eb0c1b13
claudio-germano <wantuir.germano@gmail.com>
2021-07-14 11:23:04 -0300

    try to create .war and link it to tomcat manually

--- src/main/java/com/digitalinnovationone/springboot/controller/HelloController.java
+++ src/main/java/com/digitalinnovationone/springboot/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	
 	@GetMapping("/")
 	public String helloMessage() {
-		return "Hello Claudio, this is your third Springboot project";
+		return "Hello Claudio, this is your third Springboot project!";
 	}
 
 }
